1f8433d723895e9409307f15acfaf9cae2d413cb
徐壹 <oncwnuFRgTlEzmMkIs-4ITcj04NY@git.weixin.qq.com>
2026-08-31 11:26:29 +0800

    fix: Hero/Navbar 下载按钮补缓存参数,与下载专区指向同一最新 APK

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -127,7 +127,7 @@ export function Hero() {
 
             <div className="flex flex-col sm:flex-row gap-3">
               <Button size="lg" className="text-base px-8 h-12" asChild>
-                <a href="downloads/suiyiime-latest.apk" download>
+                <a href="downloads/suiyiime-latest.apk?v=1.2-eff9775" download>
                   <Download className="mr-2 h-5 w-5" />
                   免费下载 APK
                 </a>
